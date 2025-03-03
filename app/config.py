6ec21eb1c7901de10f6f0a051aa079a34cb99c0f
Shantay Kellyman <Shantaykellyman@gmail.com>
2025-03-02 21:47:31 -0500

    Initial commit - Updated Flask Contact Form App

--- app/config.py
+++ app/config.py
@@ -6,19 +6,16 @@ load_dotenv()  # Load environment variables from .env file
 class Config(object):
     """Base Config Object"""
     DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')  # Set a safer default
+    SECRET_KEY = os.environ.get('SECRET_KEY', '18500f5e3ac327691b34bf4ef4c75b23beb6b8085aec7d00')
 
-    # Flask-Mail Configuration
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'sandbox.smtp.mailtrap.io')  # Use Mailtrap default
+    # Flask-Mail Configuration (Mailtrap)
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'sandbox.smtp.mailtrap.io')  
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 2525))  
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'b11c799e082117')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '01d28c1671a383')
+
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True').lower() == 'true'  
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'False').lower() == 'true'  
-
-
-
 
 
 
